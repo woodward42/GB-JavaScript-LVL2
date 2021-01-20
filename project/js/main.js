4a21ad665f174dc34429254be5af1aca28d17dd1
woodward <andrey.ivanov.5891@gmail.com>
2021-01-20 19:50:12 +0300

    Готовы методы добавления и удаления для корзины

--- project/js/main.js
+++ project/js/main.js
@@ -11,6 +11,7 @@ const cartProducts = document.querySelector('.cart-products')
 //универсальный класс товара, рендерится разная вёрстка, в зависимости от переданного из родителя (ProductsList или CartList) типа
 class Product {
     constructor(product, type) {
+        this.id = product.product_id    //добавил сюда id, чтобы при удалении при поиске не лезть вглубь объекта
         this.type = type;
         this.product = product; //заменил свойства на просто объект, распарсивать его будем уже в вёрстке в функции рендера
         this.isRendered = false
@@ -18,12 +19,6 @@ class Product {
 
     renderProduct(){
         this.isRendered = true
-    
-        /*
-            Мне не понравилось передавать img отдельно вторым параметром в функцию рендера
-            Я добавляю её свойством в объект, если у него нет своего свойства img
-                                                                                          
-        !this.product.hasOwnProperty('img') ? this.product.img = this.img : ''*/
         
         //в зависимости от типа создаваемого продукта рендерим ту или иную верстку
         switch(this.type){
@@ -80,7 +75,10 @@ class List {
             .then(() => this._renderProductsList())
             .then(() => {       //если это каталог, то добавляем обработчик добавления товара на кнопки каталога
                 if (this.type === 'catalog'){
-                    this._addListeners()
+                    this._addCatalogListeners()
+                }
+                else{ 
+                    this._addCartListeners()
                 }
              })
     }
@@ -116,8 +114,8 @@ class ProductsList extends List{
         super(cart, container, type, url)
     }
 
-    //функция одноименная функции в корзине, вызывать её попробую в finally после рендера в родительском классе List
-    _addListeners(){
+    //функция аналогичная функции в корзине, вызывать её попробую после рендера в родительском классе List
+    _addCatalogListeners(){
         this.container.addEventListener('click', evt => {   //обработчик вешаем на контейнер, клики поймаем через вспылтие
             
             //если кликнули по кнопке - соберем из дата атрибутов объект и отдадим его в функцию корзины add
@@ -126,8 +124,8 @@ class ProductsList extends List{
                 let newItemToAdd = {
                     product_id: newItemData.id,
                     product_title: newItemData.title,
+                    product_price: +newItemData.price,
                     product_image: newItemData.image,
-                    product_price: newItemData.price,
                 }
 
                 this.cart.add(newItemToAdd)
@@ -143,9 +141,32 @@ class Cart extends List{
         super(cart, container, type, url)
     }
  
+    _addCartListeners(){
+        this.container.addEventListener('click', evt => {   //обработчик вешаем на контейнер, клики поймаем через вспылтие
+            
+            //если кликнули по кнопке - заберем из дата атрибута id и отдадим в метод remove
+            if (evt.target.name === 'remove'){
+                this._remove(evt.target.dataset.id)
+            }
+            
+        })
+    }
+
     add(item){
         this.data.push(item)
         this.products.push(new Product(item,'cart'))
+        this._renderProductsList()
+    }
+    _remove(id){
+        //находим в каждом из массивов по id элемент, который надо удалить
+        let productToDeleteFromData = this.data.find(item => item.product_id == id)
+        let productToDeleteFromProducts = this.products.find(item => item.id == id)
+
+        //удаляем 1 элемент с найденной позиции в каждом из массивов
+        this.data.splice(this.data.indexOf(productToDeleteFromData), 1)
+        this.products.splice(this.products.indexOf(productToDeleteFromProducts), 1)
+
+        //перерендер корзины
         this._renderProductsList()
     }
 }
@@ -154,7 +175,3 @@ class Cart extends List{
 //const pl = new ProductsList();
 const cart = new Cart()
 const productsList = new ProductsList(cart)
-
-
-
-
