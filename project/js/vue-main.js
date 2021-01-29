34c0a20f5a8ba66c4ee19fd66b9024824c8e3d27
woodward <andrey.ivanov.5891@gmail.com>
2021-01-29 14:05:46 +0300

    Добавил компонент с ошибкой

--- project/js/vue-main.js
+++ project/js/vue-main.js
@@ -1,10 +1,12 @@
 import {Products} from './Products.js'
 import {Cart} from './Cart.js'
 import {Search} from './Search.js'
+import {CustomError} from './CustomError.js'
 
 //создаем объект, который передаст настройки приложения
 const Shop = {
-    components:{
+    components: {
+        CustomError,
         Products,
         Cart,
         Search
@@ -12,13 +14,18 @@ const Shop = {
     data(){
         return {
             API: 'https://raw.githubusercontent.com/woodward42/online-store-api/master/responses',
+            isError: false
         }
     },
     methods: {
         getJson(url){
             return fetch(url)
                     .then(data => data.json())
-                    .catch(error => console.log(error))
+                    .catch(error => {
+                        this.isError = true
+                        console.log(error)
+                        
+                    })
         },
     },
 };
